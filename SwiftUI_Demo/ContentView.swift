3719d5a25823af64381fe4580a8c4696aac914a4
Chrisgarlick <cgarlick94@gmail.com>
2021-02-22 17:14:17 +0000

    Initial Commit

---
+++ SwiftUI_Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Demo
+//
+//  Created by alldio on 22/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
