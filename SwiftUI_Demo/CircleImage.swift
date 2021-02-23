353fdcd58a3babe6b19df1626f506bf963784cce
Chrisgarlick <cgarlick94@gmail.com>
2021-02-23 13:15:38 +0000

    First Commit

---
+++ SwiftUI_Demo/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  SwiftUI_Demo
+//
+//  Created by alldio on 23/02/2021.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay(Circle()
+            .stroke(Color.white, lineWidth: 2))
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
